6fa570a1bd847c3363192418b0e19e534e719aea
Emil Østergaard <emil.oestergaard@theorg.com>
2021-09-03 14:03:01 +0200

    Implement user interface

--- test1.Tests/UnitTest1.cs
+++ test1.Tests/UnitTest1.cs
@@ -7,18 +7,6 @@ namespace test1.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Main_prints_hello_world()
-        {
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-
-            Program.Main(new string[0]);
-            var output = writer.GetStringBuilder().ToString().Trim();
-
-            Assert.Equal("Hello World", output);
-        }
-
-        [Fact]
         public void yearDivisibleBy4IsLeapYear()
         {
             var output = Program.IsLeapYear(2004);

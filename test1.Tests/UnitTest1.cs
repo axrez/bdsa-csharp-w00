4071f9a7e71f22c46bc62cd84c14364fb70d2b19
Emil Østergaard <emil.oestergaard@theorg.com>
2021-09-06 21:21:25 +0200

    Add unit tests for user interface

--- test1.Tests/UnitTest1.cs
+++ test1.Tests/UnitTest1.cs
@@ -7,6 +7,36 @@ namespace test1.Tests
     public class UnitTest1
     {
         [Fact]
+        public void promptUserNonLeapYearNumber()
+        {
+            var writer = new StringWriter();
+            var reader = new StringReader("1999");
+            Console.SetOut(writer);
+            Console.SetIn(reader);
+
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString().Trim().Split("\n");
+
+            Assert.Equal("Please enter a number:", output[0]);
+            Assert.Equal("nay", output[1]);
+        }
+
+        [Fact]
+        public void promptUserLeapYearNumber()
+        {
+            var writer = new StringWriter();
+            var reader = new StringReader("2004");
+            Console.SetOut(writer);
+            Console.SetIn(reader);
+
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString().Trim().Split("\n");
+
+            Assert.Equal("Please enter a number:", output[0]);
+            Assert.Equal("yay", output[1]);
+        }
+
+        [Fact]
         public void yearDivisibleBy4IsLeapYear()
         {
             var output = Program.IsLeapYear(2004);
